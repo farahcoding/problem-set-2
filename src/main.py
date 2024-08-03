7589d08245ceb6116626a90726ad8109717de730
Farah <farah.assgari@gmail.com>
2024-08-03 14:36:23 -0400

    updates to etl script

--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ You will run this problem set from main.py, so set things up accordingly
 
 import pandas as pd
 import etl
-import preprocessing
-import logistic_regression
-import decision_tree
-import calibration_plot
+#import preprocessing
+#import logistic_regression
+#import decision_tree
+#import calibration_plot
 
 
 # Call functions / instanciate objects from the .py files
 def main():
 
     # PART 1: Instanciate etl, saving the two datasets in `./data/`
-
+    etl.getData()
     # PART 2: Call functions/instanciate objects from preprocessing
 
     # PART 3: Call functions/instanciate objects from logistic_regression
